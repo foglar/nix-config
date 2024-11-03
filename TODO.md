2bfb71bfb25a756ddbbefdfb0289691bcf97ccbc
foglar <kohout.fi.2023@skola.ssps.cz>
2024-11-03 13:48:13 +0100

    todo and configedit

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODO
+
+- [ ] Wallpaper switch
+- [ ] Keyboard switch
+- [ ] Waybar fix
+- [ ] Rofi config
+- [ ] Cliphist manager fix
+- [ ] Stylix theming
+- [ ] Enabling nvidia graphics and disabling it
+- [ ] Modularity connected via configuration.nix file with home manager (disable gnome, gnome configuration will be disabled too)
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,9 +22,9 @@
   stylix = {
     enable = true;
     image = ./aurora_borealis.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     polarity = "dark";
     autoEnable = true;
 
