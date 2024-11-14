9412bdf39e69403359ffd59679c79e9d827ff1d5
foglar <kohout.fi.2023@skola.ssps.cz>
2024-11-14 10:27:55 +0100

    prettierrc fix

--- nixos/home.nix
+++ nixos/home.nix
@@ -60,7 +60,7 @@
     ".config/hypr/mocha.conf".source = ../config/mocha.conf;
     ".config/hypr/hypridle.conf".source = ../config/hypridle.conf;
     ".config/dolphinrc".source = ../config/dolphinrc;
-    ".prettierrc".source = ../config/
+    ".prettierrc".source = ../config/.prettierrc;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
