ea0b86d53b78e175ce1c174f4b505e94e3c5937b
foglar <kohout.fi.2023@skola.ssps.cz>
2024-12-09 19:14:34 +0100

    spotify

---
+++ nixos/packages/applications/spotify.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  options = {
+    program.spotify.enable = lib.mkEnableOption "enable spotify";
+  };
+
+  config = lib.mkIf config.program.spotify.enable {
+    programs.spicetify = let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
+    };
+  };
+}
