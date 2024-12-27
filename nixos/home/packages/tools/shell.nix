c2eaa3b7df2c0c72dcd158c9ac7db00418c52bae
foglar <kohout.fi.2023@skola.ssps.cz>
2024-12-27 21:36:19 +0100

    shell aliases fix for both shells same

--- nixos/home/packages/tools/shell.nix
+++ nixos/home/packages/tools/shell.nix
@@ -3,38 +3,38 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  shellAliases = {
+    ls = "${pkgs.eza}/bin/eza --icons";
+    ll = "${pkgs.eza}/bin/eza -alh --icons";
+    l = "${pkgs.eza}/bin/eza -lh  --icons=auto";
+    tree = "${pkgs.eza}/bin/eza --tree --icons";
+    open = "rifle";
+    ip = "ip -c";
+    s = "kitten ssh";
+    diff = "diff --color";
+    respawn = "clear; ${pkgs.pfetch}/bin/pfetch";
+    mkdir = "mkdir -p";
+    cat = "${pkgs.bat}/bin/bat --style plain";
+    rasp = "s masaoka";
+    hist = "history | awk '{for (i=2; i<=NF; i++) printf \$i\" \"; print \"\"}' | fzf | wl-copy";
+    cdx = "${pkgs.zoxide}/bin/zoxide query --interactive";
+    #distrobox-enter = "distrobox-enter --root";
+    #distrobox-create = "distrobox-create --root";
+    #distrobox-list = "distrobox-list --root";
+  };
+in {
   options = {
     sh.bash.enable = lib.mkEnableOption "enables shell bash";
     sh.zsh.enable = lib.mkEnableOption "enables shell zsh";
   };
-
   config = lib.mkMerge [
     (lib.mkIf config.sh.bash.enable {
       programs.bash = {
         enable = true;
         enableCompletion = true;
 
-        shellAliases = {
-          ls = "${pkgs.eza}/bin/eza --icons";
-          ll = "${pkgs.eza}/bin/eza -alh --icons";
-          l = "${pkgs.eza}/bin/eza -lh  --icons=auto";
-          tree = "${pkgs.eza}/bin/eza --tree --icons";
-          open = "rifle";
-          ip = "ip -c";
-          s = "kitten ssh";
-          diff = "diff --color";
-          respawn = "clear; ${pkgs.pfetch}/bin/pfetch";
-          mkdir = "mkdir -p";
-          cat = "${pkgs.bat}/bin/bat --style plain";
-          rasp = "s masaoka";
-          hist = "history | awk '{for (i=2; i<=NF; i++) printf \$i\" \"; print \"\"}' | fzf | wl-copy";
-          cdx = "${pkgs.zoxide}/bin/zoxide query --interactive";
-
-          #distrobox-enter = "distrobox-enter --root";
-          #distrobox-create = "distrobox-create --root";
-          #distrobox-list = "distrobox-list --root";
-        };
+        shellAliases = shellAliases;
 
         bashrcExtra = ''
           ${pkgs.pfetch}/bin/pfetch'';
@@ -52,19 +52,11 @@
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
 
-        shellAliases = {
-          ls = "${pkgs.eza}/bin/eza --icons";
-          ll = "${pkgs.eza}/bin/eza -alh --icons";
-          l = "${pkgs.eza}/bin/eza -lh  --icons=auto";
-          tree = "${pkgs.eza}/bin/eza --tree --icons";
-          open = "rifle";
-          ip = "ip -c";
-          s = "kitten ssh";
-          diff = "diff --color";
-          respawn = "clear; ${pkgs.pfetch}/bin/pfetch";
-          mkdir = "mkdir -p";
-          cat = "${pkgs.bat}/bin/bat --style plain";
-        };
+        shellAliases = shellAliases;
+
+        initExtra = ''
+          ${pkgs.pfetch}/bin/pfetch
+          set -o emacs'';
       };
     })
   ];
