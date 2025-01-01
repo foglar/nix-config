f550d7c07ff5400a433425919de6d1e7866e9c63
foglar <kohout.fi.2023@skola.ssps.cz>
2025-01-01 10:36:42 +0100

    home-manager fix

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
     };
 
     home-manager-droid = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-droid";
     };
   };
