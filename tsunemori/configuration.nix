122a8a3383401460167f7602bc23e723a34ca800
foglar <kohout.fi.2023@skola.ssps.cz>
2025-01-01 12:34:49 +0100

    font added

--- tsunemori/configuration.nix
+++ tsunemori/configuration.nix
@@ -16,6 +16,9 @@
     extraSpecialArgs = {inherit inputs userSettings;};
   };
 
+  user.userName = "${userSettings.username}";
+  terminal.font = "${pkgs.nerd-fonts.terminess-ttf}share/fonts/truetype/NerdFonts/Terminess/TerminessNerdFont-Regular.ttf";
+
   # Simply install just the packages
   environment.packages = with pkgs; [
     nano
