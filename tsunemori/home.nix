e1f423987a7f97e1b6b2d2e5bc9492cf5177b506
foglar <kohout.fi.2023@skola.ssps.cz>
2025-01-01 12:48:41 +0100

    update phone package

--- tsunemori/home.nix
+++ tsunemori/home.nix
@@ -5,5 +5,37 @@
     ../nixos/home/packages/droid-packages.nix
   ];
 
+  sh.bash = {
+    enable = true;
+    oh-my-posh.enable =
+      true;
+  };
+
+  sh.zsh = {
+    enable =
+      true;
+    oh-my-posh.enable =
+      true;
+  };
+
+  sh.oh-my-posh.enable = true;
+
+  program = {
+    tmux.enable = true;
+    zoxide.enable = true;
+    git.enable = true;
+    neovim.enable = true;
+  };
+
+  app_list = {
+    terminal_tools.enable = true;
+  };
+
+  programs = {
+    bat.enable = true;
+    btop.enable = true;
+    fzf.enable = true;
+  };
+
   programs.home-manager.enable = true;
 }
