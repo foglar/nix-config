1c2a3e34cfa46885c1bde19c5a9c12f1cb4f404a
foglar <kohout.fi.2023@skola.ssps.cz>
2025-01-01 10:46:39 +0100

    Update, home manager test

--- tsunemori/configuration.nix
+++ tsunemori/configuration.nix
@@ -1,9 +1,4 @@
 {pkgs, ...}: {
-  imports = [
-    ../nixos/system/packages.nix
-    #  ../nixos/system/system.nix
-  ];
-
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
@@ -13,49 +8,6 @@
     backupFileExtension = "hm-bak";
     useGlobalPkgs = true;
   };
-
-  #sys = {
-  #  audio.enable = false;
-  #  bootloader.systemd-boot.enable = false;
-  #  desktop = {
-  #    steamdeck.enable = false;
-  #  };
-  #  fonts.packages = false;
-  #  locales.enable = true;
-  #  network.enable = false;
-  #  bluetooth = {
-  #    enable = false;
-  #    blueman.enable = false;
-  #  };
-  #  nvidia = {
-  #    enable = false;
-  #    mode = "none";
-  #  };
-  #  printing.enable = false;
-  #  login = {
-  #    sddm.enable = false;
-  #    gdm.enable = false;
-  #  };
-  #  style.enable = false;
-  #};
-  
-  program = {
-    docker.enable = true;
-    podman.enable = false;
-    steam.enable = false;
-    proxychains.enable = false;
-    tor.enable = false;
-    virt-manager.enable = false;
-    virtualbox.enable = false;
-    yubikey = {
-      enable = false;
-      lock-on-remove = false;
-      notify = false;
-    };
-    ssh.client.enable = false;
-  };
-
-  #nixpkgs.config.allowUnfree = true;
 
   # Simply install just the packages
   environment.packages = with pkgs; [
--- tsunemori/home.nix
+++ tsunemori/home.nix
@@ -1,11 +1,71 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  # Read the changelog before changing this value
+{...}: {
+  home.username = "tsunemori";
+  home.homeDirectory = "/home/tsunemori";
   home.stateVersion = "24.05";
 
-  # insert home-manager config
+  imports = [
+    ../nixos/home/packages/packages.nix
+    ../nixos/home/desktop/desktops.nix
+  ];
+
+  # Shell management
+  sh.oh-my-posh.enable = true;
+
+  sh.bash = {
+    enable = true;
+    oh-my-posh.enable = true;
+  };
+
+  sh.zsh = {
+    enable = true;
+    oh-my-posh.enable = true;
+  };
+
+  # Application lists
+  app_list = {
+    applications.enable = false;
+    games.enable = false;
+    hacking.enable = false;
+    programming.enable = false;
+    terminal_tools.enable = true;
+  };
+
+  # Configured programs to enable
+  program = {
+    kitty.enable = false;
+    tmux.enable = true;
+    zoxide.enable = true;
+
+    vscode = {
+      enable = false;
+      ide = {
+        python.enable = false;
+        go.enable = false;
+        csharp.enable = false;
+        cpp.enable = false;
+        web.enable = false;
+      };
+      nix.enable = false;
+      markdown.enable = false;
+      ai.enable = false;
+      git.enable = false;
+
+      themes.enable = false;
+    };
+
+    git.enable = true;
+    neovim.enable = true;
+
+    firefox.enable = false;
+    spotify.enable = false;
+  };
+
+  # Basic programs to enable
+  programs = {
+    bat.enable = true;
+    btop.enable = true;
+    fzf.enable = true;
+  };
+
+  programs.home-manager.enable = true;
 }
