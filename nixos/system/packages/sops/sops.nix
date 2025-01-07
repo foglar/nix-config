2095205ab5fbf0a74a2e587993745afe2fedc0a0
foglar <kohout.fi.2023@skola.ssps.cz>
2025-01-07 18:25:24 +0100

    submit secrets and sops test

--- nixos/system/packages/sops/sops.nix
+++ nixos/system/packages/sops/sops.nix
@@ -2,7 +2,6 @@
   lib,
   config,
   userSettings,
-  pkgs,
   ...
 }: {
   options = {
@@ -10,25 +9,32 @@
   };
 
   config = lib.mkIf config.sys.security.sops.enable {
-    sops.defaultSopsFile = ./secrets/secrets.yaml;
-    sops.defaultSopsFormat = "yaml";
+    sops = {
+      defaultSopsFile = ./secrets/secrets.yaml;
+      defaultSopsFormat = "yaml";
 
-    sops.age.keyFile = "/home/${userSettings.username}/.config/sops/age/keys.txt";
+      age.keyFile = "/home/${userSettings.username}/.config/sops/age/keys.txt";
 
-    sops.secrets."${userSettings.hostname}/password-hash" = {
-      neededForUsers = true;
-    };
+      # SSH private keys
+      secrets = {
+        "ssh_keys/masaoka_private" = {
+          path = "/home/${userSettings.username}/.ssh/id_masaoka";
+          owner = userSettings.username;
+          group = "users";
+        };
 
-    environment.systemPackages = with pkgs; [
-      sops
-    ];
+        "${userSettings.hostname}/password-hash" = {
+          neededForUsers = true;
+        };
 
-    # SSH private keys
-    sops.secrets = {
-      "ssh_keys/masaoka_private" = {
-        path = "/home/${userSettings.username}/.ssh/id_masaoka";
-        owner = userSettings.username;
-        group = "users";
+        "${userSettings.hostname}/syncthing" = {
+          owner = userSettings.username;
+          group = "users";
+        };
+
+        templates = {
+          "${userSettings.hostname}/syncthing".content = ''${config.sops.placeholder."${userSettings.hostname}/syncthing"}'';
+        };
       };
     };
 
@@ -36,15 +42,14 @@
     users.users.${userSettings.username}.hashedPasswordFile = "${config.sops.secrets."${userSettings.hostname}/password-hash".path}";
 
     # YubiKey IDs
-    security.pam.yubico.id =
-      []
-      ++ (
-        if config.program.yubikey.enable
-        then ["${config.sops.secrets.yubikey_id}".value]
-        else []
-      );
+    security.pam.yubico.id = [17032113];
+    #++ (
+    #  if config.program.yubikey.enable
+    #  then ["${config.sops.secrets.yubikey_id}".value]
+    #  else []
+    #);
 
     # Syncthing password
-    services.syncthing.settings.gui.password = "${config.sops.secrets."syncthing".value}";
+    #services.syncthing.settings.gui.password = config.sops.templates."${userSettings.hostname}/syncthing".content;
   };
 }
